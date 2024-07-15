193459eea1c249250228db33bcfb44b1d4cb6652
vgabriel99 <vgabrieldev@gmail.com>
2024-07-15 17:36:56 -0300

    get /pessoas totalmente refatorado

---
+++ src/routes/pessoaRoutes.js
@@ -0,0 +1,6 @@
+import express from "express"; 
+import PessoaController from "../controller/pessoaController.js"; 
+
+const routes = express.Router();
+
+routes.get("/pessoas",PessoaController.listarPessoas);
