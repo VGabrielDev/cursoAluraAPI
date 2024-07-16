b17bacc723bfbcbce7df9b9bb8dffd4ca1c4c0b6
vgabriel99 <vgabrieldev@gmail.com>
2024-07-16 16:33:35 -0300

    Corrijindo código

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,12 @@ import express from "express";
 import pessoas from "./pessoaRoutes.js";
 
 const routes = (app) => {
-app.routes("/").get((req, res) => res.status(200).send("Curso de Node.js"))
-};
+app.route("/").get((req, res) => res.status(200).send("Curso de Node.js"))
 
 app.use(express.json(), pessoas)
+
+};
+
+
 
 export default routes;
