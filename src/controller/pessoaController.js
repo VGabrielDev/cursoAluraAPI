64827b9c66aa397b5ab424d013d54b9749cc5a89
vgabriel99 <vgabrieldev@gmail.com>
2024-07-16 14:57:23 -0300

    Refatorando post

--- src/controller/pessoaController.js
+++ src/controller/pessoaController.js
@@ -6,6 +6,15 @@ class PessoaController {
         const listaPessoas = await pessoas.find({});
         res.status(200).json(listaPessoas);
     }
+
+    static async cadastrarPessoa(req, res){
+     try{
+        const novaPessoa = await pessoas.create(req.body); 
+        res.status(201).json({message: "cadastrado com sucesso", pessoas:novaPessoa});
+     }catch(erro){
+        res.status(500).json({message: `${erro.message} - falha ao cadastrar livro`});
+     }
+    }
 };
 
 export default PessoaController;
