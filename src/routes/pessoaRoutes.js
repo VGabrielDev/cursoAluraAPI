6e3beda3f3c39302ce2028a6d7d0b03da87c111c
vgabriel99 <vgabrieldev@gmail.com>
2024-07-17 10:31:44 -0300

    Atualizando rotas

--- src/routes/pessoaRoutes.js
+++ src/routes/pessoaRoutes.js
@@ -4,5 +4,8 @@ import PessoaController from "../controller/pessoaController.js";
 const routes = express.Router();
 
 routes.get("/pessoas",PessoaController.listarPessoas);
+routes.get("/pessoas/:id",PessoaController.listarPessoasPorId);
+routes.post("/pessoas",PessoaController.cadastrarPessoa);
+routes.put("/pessoas/:id",PessoaController.atualizarPessoa);
 
 export default routes;
