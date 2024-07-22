9407d7495b79d99b160d74c84343df4af69e7aa6
vgabriel99 <vgabrieldev@gmail.com>
2024-07-22 20:38:27 -0300

    Criando model de crianças

---
+++ src/models/crianca.js
@@ -0,0 +1,12 @@
+import { MongoOIDCError } from "mongodb";
+import mongoose from "mongoose";
+
+const criancasSchema = new mongoose.Schema({
+    id: {type: mongoose.Schema.Types.ObjectId},
+    nome : {type: String, required: true},
+    idade : {type: Number},
+}, {versionKey: false});
+
+const criancas = mongoose.model("criancas", criancasSchema);
+
+export default criancas; 
