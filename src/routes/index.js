60c9d3bc4de2cb597ba6e60958c84b0994138c91
vgabriel99 <vgabrieldev@gmail.com>
2024-07-22 21:42:41 -0300

    Adicionando rotas de crianças no index das rotas

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import express from "express";
 import pessoas from "./pessoaRoutes.js";
+import criancas from "../models/Crianca.js";
 
 const routes = (app) => {
 app.route("/").get((req, res) => res.status(200).send("Curso de Node.js"))
 
-app.use(express.json(), pessoas)
+app.use(express.json(), pessoas, criancas)
 
 };
 
