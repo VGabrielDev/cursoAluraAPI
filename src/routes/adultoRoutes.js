51ebbb8c9172ee348a4f64cad763a06126316905
vgabriel99 <vgabrieldev@gmail.com>
2024-07-24 12:52:47 -0300

    Adicionando rota da busca de adultos por id de criança

--- src/routes/adultoRoutes.js
+++ src/routes/adultoRoutes.js
@@ -4,6 +4,7 @@ import adultoController from "../controller/adultoController.js";
 const routes = express.Router();
 
 routes.get("/adultos",adultoController.listarAdultos);
+routes.get("/adultos/busca",adultoController.listarAdultosPorIdCrianca);
 routes.get("/adultos/:id",adultoController.listarAdultosPorId);
 routes.post("/adultos",adultoController.cadastrarAdulto);
 routes.put("/adultos/:id",adultoController.atualizarAdulto);
